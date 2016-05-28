43de28521f1cbb3028f5420464df00580d62a368
Carlos <conticarlos91@gmail.com>
2016-05-29 01:36:56 +0300

    assignment 36 complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
   let (:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
+  it { is_expected.to allow_value("Steve Jobs").for(:name) }
 
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
@@ -24,6 +25,7 @@ RSpec.describe User, type: :model do
   describe "invalid user" do
     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
     let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
+    let(:user_with_downcased_name) { User.new(name: "steve jobs", email: "user@bloccit.com")}
 
     it "should be an invalid user due to blank name" do
       expect(user_with_invalid_name).to_not be_valid
@@ -33,5 +35,8 @@ RSpec.describe User, type: :model do
       expect(user_with_invalid_email).to_not be_valid
     end
 
+    it "should be an invalid user name due to downcased letters" do
+      expect(user_with_downcased_name).to_not be_valid
+    end
   end
 end
